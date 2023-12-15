9ff7a5eb3de5e7a9dc3dabfb42b1af1ac2a7a629
TrAdOri <evjeuland04@gmail.com>
2023-12-15 12:24:59 +0100

    add: Creation de la page register et de son css

--- views/RegisterView.php
+++ views/RegisterView.php
@@ -12,7 +12,17 @@
   <main>
 
     <form action="api/auth/login.php" method="post">
-      <h1>Se connecter a Game Collection</h1>
+      <h1>Inscription</h1>
+
+      <div class="form-item">
+        <label for="name">Nom :</label>
+        <input type="name" name="name" id="name" placeholder="Nom" require>
+      </div>
+
+      <div class="form-item">
+        <label for="firstname">Prenom :</label>
+        <input type="firstname" name="firstname" id="firstname" placeholder="Prenom" require>
+      </div>
 
       <div class="form-item">
         <label for="email">Email :</label>
@@ -24,10 +34,15 @@
         <input type="password" name="password" id="password" placeholder="Mot de passe" require minlength="8">
       </div>
 
-      <input type="submit" value="Se connecter">
+      <div class="form-item">
+        <label for="passwordverif">Confirmation du mot de passe :</label>
+        <input type="passwordverif" name="passwordverif" id="passwordverif" placeholder="Mot de passe" require minlength="8">
+      </div>
+
+      <input type="submit" value="S'inscrire">
     </form>
 
-    <a href="register">S'inscrire</a>
+    <a href="register">Se connecter</a>
   </main>
 </body>
 
