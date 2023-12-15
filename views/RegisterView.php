ac39785d0078b993f9bb8e0624a16956e67a8d74
TrAdOri <evjeuland04@gmail.com>
2023-12-15 12:22:29 +0100

    add: Creation de la page register et du controller register et de la vue register et du css register

---
+++ views/RegisterView.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/pages/login.css">
+  <title>Game Collection</title>
+</head>
+
+<body>
+  <main>
+
+    <form action="api/auth/login.php" method="post">
+      <h1>Se connecter a Game Collection</h1>
+
+      <div class="form-item">
+        <label for="email">Email :</label>
+        <input type="email" name="email" id="email" placeholder="Email" require>
+      </div>
+
+      <div class="form-item">
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" id="password" placeholder="Mot de passe" require minlength="8">
+      </div>
+
+      <input type="submit" value="Se connecter">
+    </form>
+
+    <a href="register">S'inscrire</a>
+  </main>
+</body>
+
+</html>
