0b26c56b5eec86e7319364fa37b6f58230532b25
aBgAmeuR <a.josset@yahoo.com>
2023-12-15 10:48:20 +0100

    Update language and add CSS stylesheet

--- src/Views/HomeView.php
+++ src/Views/HomeView.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="src/assets/css/index.css">
+  <title>Game Collection</title>
 </head>
+
 <body>
   <h1>Home page</h1>
 </body>
+
 </html>
