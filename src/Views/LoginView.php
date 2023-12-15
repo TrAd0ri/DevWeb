d79e3858d00603ce0fd70aec571b35a0f9376316
aBgAmeuR <a.josset@yahoo.com>
2023-12-15 12:00:02 +0100

    feat: auth login

--- src/Views/LoginView.php
+++ src/Views/LoginView.php
@@ -4,26 +4,27 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="src/assets/css/index.css">
+  <link rel="stylesheet" href="src/assets/css/pages/login.css">
   <title>Game Collection</title>
 </head>
 
 <body>
   <main>
-    <h1>Se connecter a Game Collection</h1>
 
-    <form action="" method="post">
-      <div>
+    <form action="api/auth/login.php" method="post">
+      <h1>Se connecter a Game Collection</h1>
+
+      <div class="form-item">
         <label for="email">Email :</label>
         <input type="email" name="email" id="email" placeholder="Email" require>
       </div>
-      <div>
+
+      <div class="form-item">
         <label for="password">Mot de passe :</label>
         <input type="password" name="password" id="password" placeholder="Mot de passe" require minlength="8">
       </div>
-      <div>
-        <input type="submit" value="Se connecter">
-      </div>
+
+      <input type="submit" value="Se connecter">
     </form>
 
     <a href="register">S'inscrire</a>
