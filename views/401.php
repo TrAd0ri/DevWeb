5a472e07c98f1ade4575a1bfcc4fc9c7f69ab504
aBgAmeuR <a.josset@yahoo.com>
2023-12-20 15:51:17 +0100

    fix: Update button styles and fix redirection in HomeController

--- views/401.php
+++ views/401.php
@@ -4,12 +4,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/pages/404.css">
+  <link rel="stylesheet" href="assets/css/index.css">
+  <link rel="stylesheet" href="assets/css/pages/401.css">
   <title>Game Collection</title>
 </head>
 
 <body>
-  <h1>404</h1>
+  <h1>401</h1>
+  <button><a href="login">Se connecter</a></button>
 </body>
 
 </html>
