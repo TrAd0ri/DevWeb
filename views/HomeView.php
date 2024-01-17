a6f6f3960cf980c467787a9bd31de4a97c9ee887
aBgAmeuR <a.josset@yahoo.com>
2024-01-17 22:15:54 +0100

    style: header and games list

--- views/HomeView.php
+++ views/HomeView.php
@@ -10,36 +10,45 @@
 
 <body>
   <?php include 'components/Header.php'; ?>
+
   <main>
-  <section class="presentation">
-    <div class="home-image">
-      <img src="assets\images\Fond_Home.jpg" >
-    </div>
-    <h1 class="home_sentence">Salut <?= $_SESSION['user_surname']?> ! Prêt à ajouter des jeux à ta collection ?</h1>
-  </section>
-  <div class="games">
+    <section class="presentation">
+      <div class="home-image-container">
+        <img src="assets\images\Fond_Home.jpg">
+      </div>
+
+      <h1>Salut
+        <?= $_SESSION['user_surname'] ?> ! Prêt à ajouter des jeux à ta collection ?
+      </h1>
+    </section>
+
+    <h2 class="games-title">Mes jeux</h2>
+
+    <section class="games">
       <?php foreach ($games as $game): ?>
-        <div class="game">
+        <a class="game" href="game?id=<?= $game['id'] ?>">
           <div class="game-image">
             <img src="<?= $game['url_image'] ?>" alt="<?= $game['name'] ?>">
           </div>
           <div class="game-content">
-            <h2>
-              <?= $game['name']," ", $game['hoursPlayed'] ?> h
-            </h2>
+            <div class="game-content-header">
+              <h2>
+                <?= $game['name'] ?>
+              </h2>
+              <p>
+                <?= $game['hoursPlayed'] . 'h' ?>
+              </p>
+            </div>
             <p>
-              <?php foreach ($game['platforms'] as $index => $platform): ?>
-                <?= $platform ?>
-                <?php if ($index < count($game['platforms']) - 1): ?>
-                  ,
-                <?php endif; ?>
-              <?php endforeach; ?>
+              <?= implode(", ", $game['platforms']) ?>
             </p>
           </div>
-        </div>
+        </a>
       <?php endforeach; ?>
-    </div>
-      </main>
+    </section>
+  </main>
+
+  <?php include 'components/Footer.php'; ?>
 </body>
 
 </html>
